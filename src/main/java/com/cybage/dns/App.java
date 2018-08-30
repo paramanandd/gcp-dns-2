5812c850cb19ad80c17337679f1707904029c214
Parma <paramanandd@gmail.com>
2018-08-30 13:37:39 +0530

    Added all files

--- src/main/java/com/cybage/dns/App.java
+++ src/main/java/com/cybage/dns/App.java
@@ -29,45 +29,51 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
+import java.util.List;
+// for new code 
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.dns.model.ResourceRecordSet;
+import com.google.api.services.dns.model.ResourceRecordSetsListResponse;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Arrays;
 public class App 
 {
     public static void main( String[] args )
     {
-/**Iterator<RecordSet> recordSetIterator = zone.listRecordSets().iterateAll();
-    	    System.out.println( "Hello World!" );
-	Dns dns = DnsOptions.getDefaultInstance().getService();
-	// List all your zones
-Iterator<Zone> zoneIterator = dns.listZones().iterateAll();
-int counter = 1;
-while (zoneIterator.hasNext()) {
-  System.out.printf("#%d.: %s%n%n", counter, zoneIterator.next());
-  counter++;
-}
+	Dns dns = DnsOptions.getDefaultInstance().getService();	
+	// Create a zone metadata object
+	String zoneName = "jellyfish-zone"; // Change this zone name which is unique within your project
+	String domainName = "domain.com."; // Change this to a domain which you own
+	String description = "This is a google-cloud-dns sample zone.";
+	ZoneInfo zoneInfo = ZoneInfo.of(zoneName, domainName, description);
+//	List<Zone> zonelist;
+	//create zone
+/**	Zone zone = dns.create(zoneInfo);
+	System.out.printf("Zone was created and assigned ID %s.%n", zone.getGeneratedId());
 
-// List the record sets in a particular zone
-recordSetIterator = zone.listRecordSets().iterateAll();
-System.out.println(String.format("Record sets inside %s:", zone.getName()));
-while (recordSetIterator.hasNext()) {
-  System.out.println(recordSetIterator.next());
-}*/
-//    Dns dns;
-  //  Zone zone=null;
-    
-    Dns  dns = DnsOptions.getDefaultInstance().getService();
-    Iterator<Zone> zoneIterator = dns.listZones().iterateAll().iterator();
-      if (zoneIterator.hasNext()) {
-        System.out.println("The project contains the following zones:");
-        while (zoneIterator.hasNext()) {
-         // printZone(zoneIterator.next());
-//	  System.out.printf("%nName: %s%n", zone.getName();
-         System.out.printf("%nName: %s%n", zoneIterator.next().getName());
-	//Iterator<RecordSet> recordSetIterator = zoneIterator.next().listRecordSets().iterateAll();
-	//while (recordSetIterator.hasNext()) {
-	 // System.out.println(recordSetIterator.next());
-//	}
+	//list name servers
+	List<String> nameServers = zone.getNameServers();
+	for (String nameServer : nameServers) {
+	  System.out.println(nameServer);
 	}
-      } else {
-        System.out.println("Project contains no zones.");
-      }
-}
+*/
+/**	Iterator<Zone> zoneIterator = dns.listZones().iterateAll();
+	int counter = 1;
+	while (zoneIterator.hasNext()) {
+		zonelist.add(zoneIterator.next());
+	      	System.out.printf("#%d.: %s%n%n", counter, zoneIterator.next());
+	  counter++;
+	} */
+	//List<Zone> zones=dns.listZones();
+//	for(Zone zone : zones){
+//	System.out.printf("name : ",zone.getName());
+//	} 
+	Zone zone =dns.getZone(zoneName);
+	System.out.printf("Record sets inside %s:", zone.getName());
+    }
 }
